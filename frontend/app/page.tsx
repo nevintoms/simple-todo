bcdc7fe1f3b094ade2e29ce0d486364314e6be5e
Nevin Toms <nevintoms333@gmail.com>
2023-12-22 13:20:13 -0500

    Integrate frontend with backend

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,40 +1,66 @@
 "use client";
 
-import { useState } from 'react';
 import Button from '@/components/button/Button';
+import { useState, useEffect } from 'react';
 
 export default function Home() {
   const [inputText, setInputText] = useState('');
   const [todoList, setTodoList] = useState([]);
 
+  useEffect(() => {
+    // Fetch all todos from Flask backend on component mount
+    fetch('http://localhost:5000/todos')
+      .then((response) => response.json())
+      .then((data) => setTodoList(data))
+      .catch((error) => console.error('Error fetching todos:', error));
+  }, []);
+
   const handleAdd = () => {
     if (inputText.trim() !== '') {
-      setTodoList([...todoList, inputText]);
+      // Add a new todo to Flask backend
+      fetch('http://localhost:5000/todos', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ task: inputText }),
+      })
+        .then((response) => response.json())
+        .then((data) => setTodoList([...todoList, data]))
+        .catch((error) => console.error('Error adding todo:', error));
+
       setInputText('');
     }
   };
 
-  const handleEdit = (index) => {
-    const updatedText = prompt('Edit item:', todoList[index]);
-    if (updatedText !== null) {
-      const updatedList = [...todoList];
-      updatedList[index] = updatedText;
-      setTodoList(updatedList);
-    }
+  const handleEdit = (index, updatedText) => {
+    // Update a todo in Flask backend
+    fetch(`http://localhost:5000/todos/${todoList[index].id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ task: updatedText }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        const updatedList = [...todoList];
+        updatedList[index] = data;
+        setTodoList(updatedList);
+      })
+      .catch((error) => console.error('Error updating todo:', error));
   };
 
   const handleDelete = (index) => {
     const confirmDelete = window.confirm('Are you sure you want to delete this item?');
     if (confirmDelete) {
-      const updatedList = [...todoList];
-      updatedList.splice(index, 1);
-      setTodoList(updatedList);
-    }
-  };
-
-  const handleKeyDown = (e) => {
-    if (e.key === 'Enter') {
-      handleAdd();
+      const todoId = todoList[index].id;
+      // Delete a todo in Flask backend
+      fetch(`http://localhost:5000/todos/${todoId}`, {
+        method: 'DELETE',
+      })
+        .then(() => setTodoList(todoList.filter((todo) => todo.id !== todoId)))
+        .catch((error) => console.error('Error deleting todo:', error));
     }
   };
 
@@ -43,23 +69,23 @@ export default function Home() {
       <h1 className="text-lg font-bold my-6">TODO</h1>
       <div className="flex">
         <input
-          className="text-black rounded-xl"
+          className="text-black rounded-xl px-2"
           type="text"
           value={inputText}
           onChange={(e) => setInputText(e.target.value)}
-          onKeyDown={handleKeyDown}
+          onKeyDown={(e) => e.key === 'Enter' && handleAdd()}
         />
-        <Button text="Add" handleOnClick={handleAdd} />
+        <Button text="Add"
+          handleOnClick={handleAdd}
+        />
       </div>
       <div className="pl-5 mt-6">
-        <ol className="list-decimal space-y-3 ">
+        <ol className="list-decimal">
           {todoList.map((item, index) => (
-            <li key={index}>
-              <div className="flex">
-                {item}
-                <Button text="Edit" handleOnClick={() => handleEdit(index)} />
-                <Button text="Delete" handleOnClick={() => handleDelete(index)} />
-              </div>
+            <li className="flex" key={item.id}>
+              {item.id}. {item.task}
+              <Button text="Edit" handleOnClick={() => handleEdit(index, prompt('Edit item:', item.task))} />
+              <Button text="Delete" handleOnClick={() => handleDelete(index)} />
             </li>
           ))}
         </ol>
